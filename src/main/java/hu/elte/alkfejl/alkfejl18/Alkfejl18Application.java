baf28edc58f26b51ff12cbf3e4e4f947b737f10f
Dorogi Benjamin <dbenjamin@inf.elte.hu>
2018-10-15 12:27:28 +0200

    created directorites, User entity

---
+++ src/main/java/hu/elte/alkfejl/alkfejl18/Alkfejl18Application.java
@@ -0,0 +1,12 @@
+package hu.elte.alkfejl.alkfejl18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Alkfejl18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Alkfejl18Application.class, args);
+	}
+}
