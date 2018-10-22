4c74fa4ecfa420d56e65bd5a038d2657c51caeea
Dorogi Benjamin <dbenjamin@inf.elte.hu>
2018-10-22 15:58:19 +0200

    UserController imports fixed

--- src/main/java/hu/elte/alkfejl/alkfejl18/controllers/UserController.java
+++ src/main/java/hu/elte/alkfejl/alkfejl18/controllers/UserController.java
@@ -1,11 +1,25 @@
 package hu.elte.alkfejl.alkfejl18.controllers;
 
+import java.util.Optional;
+import hu.elte.alkfejl.alkfejl18.entities.*;
+import hu.elte.alkfejl.alkfejl18.repositories.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
     
     @GetMapping("")
     public ResponseEntity<Iterable<User>> getAll() {
@@ -20,8 +34,6 @@ public class UserController {
             return ResponseEntity.badRequest().build();
         }
         user.setId(null);
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRole(User.Role.ROLE_USER);
         return ResponseEntity.ok(userRepository.save(user));
     }
         
