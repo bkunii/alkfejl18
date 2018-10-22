bfb477feb71a7f3936cc18b0c68f2970ee9279e7
Dorogi Benjamin <dbenjamin@inf.elte.hu>
2018-10-22 16:31:57 +0200

    ProjectController imports fixed

--- src/main/java/hu/elte/alkfejl/alkfejl18/controllers/ProjectController.java
+++ src/main/java/hu/elte/alkfejl/alkfejl18/controllers/ProjectController.java
@@ -1,13 +1,26 @@
 package hu.elte.alkfejl.alkfejl18.controllers;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import hu.elte.alkfejl.alkfejl18.entities.*;
+import hu.elte.alkfejl.alkfejl18.repositories.*;
+
 
 @RestController
 @RequestMapping("/project")
-public class SkillController {
+public class ProjectController {
     @Autowired
-    private UserRepository projectRepository;
+    private ProjectRepository projectRepository;
     
     @GetMapping("")
     public ResponseEntity<Iterable<Project>> getAll() {
@@ -32,7 +45,7 @@ public class SkillController {
     }
     
     @DeleteMapping("/{id}")
-    public ResponseEntity delete(@PathVariable Integer id) {
+    public ResponseEntity<Project> delete(@PathVariable Integer id) {
         Optional<Project> project = projectRepository.findById(id);
         if (!project.isPresent()) {
             return ResponseEntity.notFound().build();   
@@ -43,9 +56,9 @@ public class SkillController {
     }
      
     @PutMapping("/edit/{id}")
-    public ResponseEntity<Project> put(@PathVariable Integer id, @RequestBody Project user) {
-        Optional<Project> project = projectRepository.findById(id);
-        if (!project.isPresent()) {
+    public ResponseEntity<Project> put(@PathVariable Integer id, @RequestBody Project project) {
+        Optional<Project> oProject = projectRepository.findById(id);
+        if (!oProject.isPresent()) {
             return ResponseEntity.notFound().build();
         }
         
