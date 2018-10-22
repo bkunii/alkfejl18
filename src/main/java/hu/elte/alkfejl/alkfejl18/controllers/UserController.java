faea23245511e2ba94e8d7b52f39002dfe921888
AKCKD0 <AKCKD0@users.noreply.github.com>
2018-10-22 15:34:41 +0200

    Controllers - initial

---
+++ src/main/java/hu/elte/alkfejl/alkfejl18/controllers/UserController.java
@@ -0,0 +1,11 @@
+package hu.elte.alkfejl.alkfejl18.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    
+    
+}
